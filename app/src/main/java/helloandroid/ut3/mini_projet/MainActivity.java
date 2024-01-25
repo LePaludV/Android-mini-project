603291badbb0b583b7dcce35e403006c8f4e79e3
vlepalud <valentin.lepalud@gmal.com>
2024-01-25 11:45:07 +0100

    add services get all restaurants

--- app/src/main/java/helloandroid/ut3/mini_projet/MainActivity.java
+++ app/src/main/java/helloandroid/ut3/mini_projet/MainActivity.java
@@ -4,10 +4,14 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import helloandroid.ut3.mini_projet.services.RestaurantsService;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        RestaurantsService r= new RestaurantsService();
+        r.getAllRestaurants().toString();
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
---
+++ app/src/main/java/helloandroid/ut3/mini_projet/models/Restaurant.java
@@ -0,0 +1,67 @@
+package helloandroid.ut3.mini_projet.models;
+
+import com.google.firebase.firestore.GeoPoint;
+
+import java.util.ArrayList;
+
+public class Restaurant {
+
+    String id;
+    String nom;
+    String address;
+    String[] photos;
+    GeoPoint coordinates;
+    String description;
+    String horaire;
+    String type;
+    float note;
+
+
+    public String getId() {
+        return id;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public String[] getPhotos() {
+        return photos;
+    }
+
+    public GeoPoint getCoordinates() {
+        return coordinates;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getHoraire() {
+        return horaire;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public float getNote() {
+        return note;
+    }
+
+    public Restaurant(String id, String nom, String address, String[] photos, GeoPoint coordinates, String description, String horaire, String type, float note) {
+        this.id = id;
+        this.nom = nom;
+        this.address = address;
+        this.photos = photos;
+        this.coordinates = coordinates;
+        this.description = description;
+        this.horaire = horaire;
+        this.type = type;
+        this.note = note;
+    }
+}
